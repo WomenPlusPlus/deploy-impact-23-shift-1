38d3fdc53a11ca6e588a496f16d3e419e4e1b11f
Kate <92258556+galaxxyz@users.noreply.github.com>
2023-10-01 16:59:04 +0200

    Add CD workflow

--- back-end/backend/backend/settings.py
+++ back-end/backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-alf_s)m3d0ye96b9a_#t6k-uf45_kl-+43n!a=qn!#b9!dvdf7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shift-backend-prod.fly.dev']
 
 
 # Application definition
