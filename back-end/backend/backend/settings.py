7c30ba1f74531481199f56b12e58bb90b1b444f9
moliveri1618 <lastiada1@gmail.com>
2023-10-04 19:32:34 +0100

    adding rest frameowkr auth token

--- back-end/backend/backend/settings.py
+++ back-end/backend/backend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'backend_project',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
