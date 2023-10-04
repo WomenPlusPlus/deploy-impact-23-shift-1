5fda35b537eda1f4cc3114d54597d82fcfe522f5
moliveri1618 <lastiada1@gmail.com>
2023-10-04 22:04:44 +0100

    log in back end

--- back-end/backend/backend_project/views.py
+++ back-end/backend/backend_project/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.authtoken.models import Token
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from backend.serializers import UserSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.s
 def homepage(request):
@@ -19,12 +27,27 @@ def getData(request):
 
 @api_view(['POST'])
 def login(request):
-    return Response({})
+    user = get_object_or_404(User, username=request.data['username'])
+    if not user.check_password(request.data['password']): #if psw false
+        return Response("missing user", status=status.HTTP_404_NOT_FOUND)
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(user)
+    return Response({'token': token.key, 'user': serializer.data})
 
 @api_view(['POST'])
 def signup(request):
-    return Response({})
+    serializer = UserSerializer(data=request.data) # convert complex data type into python dict
+    if serializer.is_valid():
+        serializer.save() #save usrr into db
+        user = User.objects.get(username=request.data['username']) #get data for that user
+        user.set_password(request.data['password'])
+        user.save()
+        token = Token.objects.create(user=user) #create token for that user
+        return Response({"token": token.key, "user": serializer.data})
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['POST'])
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def test_token(request):
-    return Response({})
+    return Response("passed!")
