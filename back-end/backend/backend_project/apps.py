51ced55db9aff0bc2660ce495eba1a337643bfd3
majobasgall <majobasgall@users.noreply.github.com>
2023-10-28 16:41:04 +0200

    matching candidate job using POST

--- back-end/backend/backend_project/apps.py
+++ back-end/backend/backend_project/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
-
 class BackendProjectConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'backend_project'
+
+    # def ready(self):#
